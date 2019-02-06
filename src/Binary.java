d6ef414611c1307b704a9120770e5e64bb847f27
Sergei Korogod <Zenitech4@gmail.com>
2019-02-07 00:01:29 +0200

    Create algorythm Binary

--- src/Binary.java
+++ src/Binary.java
@@ -1,2 +1,25 @@
 public class Binary {
+    public static void main(String[] args) {
+        int result=binarySearch(new int[]{1,4,5,7},4);
+        if(result==-1){
+            System.out.println("Здесь нет такого числа");
+        }else {
+            System.out.println(result);
+        }
+        }
+    private static int binarySearch(int[]a,int key){
+        int low=0;
+        int high=a.length-1;
+        while(low<=high){
+             int middle=low+(high-low)/2;
+             if(key<a[middle]){
+                 high=middle-1;
+             } else if (key > a[middle]){
+                 low=middle+1;
+             }else{ return middle;
+             }
+        }
+        return -1;
+    }
+
 }
